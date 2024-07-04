55189a297b655403b6fedaa3633b023a03c197ae
Alejandro <soutofernandezalejandro@gmail.com>
2024-07-04 20:08:50 +0200

    modificado

--- js/music-player.js
+++ js/music-player.js
@@ -1,6 +1,7 @@
 const audioPlayer = document.getElementById('music-player');
 const sources = audioPlayer.getElementsByTagName('source');
 let currentTrackIndex = 0;
+const currentSongElement = document.getElementById('current-song');
 
 function playPause() {
     if (audioPlayer.paused) {
@@ -13,9 +14,19 @@ function nextTrack() {
     currentTrackIndex = (currentTrackIndex + 1) % sources.length;
     audioPlayer.src = sources[currentTrackIndex].src;
     audioPlayer.play();
+    updateCurrentSongName();
 }
+
 function previousTrack() {
     currentTrackIndex = (currentTrackIndex - 1 + sources.length) % sources.length;
     audioPlayer.src = sources[currentTrackIndex].src;
     audioPlayer.play();
+    updateCurrentSongName();
 }
+
+function updateCurrentSongName() {
+    const currentSongName = sources[currentTrackIndex].src.split('/').pop();
+    currentSongElement.textContent = `Canción actual: ${currentSongName}`;
+}
+
+window.onload = updateCurrentSongName;
