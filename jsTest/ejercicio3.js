6e624242c0672a0dc847cb1473e6c7fcfa59ccb8
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2024-07-11 17:20:27 +0200

    ejercicio3 js

---
+++ jsTest/ejercicio3.js
@@ -0,0 +1,7 @@
+var cadena1 = "cadena1";
+var position = cadena1.indexOf("ena");
+var position1 = cadena1.lastIndexOf("1");
+var minus = cadena1.toLocaleLowerCase();
+var newCadena = cadena1.replace("de", "me");
+var Cadena2 = "Cadena2";
+var superCadena = cadena1 + Cadena2;
