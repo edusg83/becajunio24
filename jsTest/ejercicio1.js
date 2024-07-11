f377ad7f9cbe6d2ecc3f65125c5a479538623169
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2024-07-11 16:47:21 +0200

    js test

---
+++ jsTest/ejercicio1.js
@@ -0,0 +1,13 @@
+console.log("MENSAJE DE INFO");
+console.info("MENSAJE INFO");
+console.dir();
+console.assert("Mensaje de error");
+console.time;
+console.timeEnd;
+console.log(console.time + "tiempo");
+
+
+var num = 69;
+var mensaje = "test";
+var enabled = true;
+var hoy = new Date();
---
+++ jsTest/ejercicio2.js
@@ -0,0 +1,7 @@
+var alto = 12;
+var ancho = 12.203004;
+var altoPorAncho = alto * ancho;
+
+console.log(altoPorAncho);
+var numRedondo = altoPorAncho.toFixed(2);
+console.log(numRedondo);
