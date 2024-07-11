39307616a6fd5ba5fad274277b42dfda5cb1ac52
aann_2 <ana.aguilera.lopez02@gmail.com>
2024-07-11 17:17:57 +0200

    javascript

---
+++ javascript/js/ejercicio3.js
@@ -0,0 +1,18 @@
+/* a */
+let cadena1 ="CADENA1";
+
+/* b */
+console.log(cadena1.indexOf("ENA"));
+
+/* c */
+console.log('Ultimo caracter: ', cadena1.charAt(-1));
+
+/* d */
+console.log(cadena1.toLowerCase);
+
+/* e */
+console.log(cadena1.replace("DE","ME"));
+
+/* f */
+let cadena2 ="CADENA2";
+console.log(cadena2+cadena1);
