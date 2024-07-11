c492373b059583edb435d25b69c1e978fac809b3
Alejandro <soutofernandezalejandro@gmail.com>
2024-07-11 16:33:27 +0200

    ejercicios

---
+++ EjerciciosJavascript/js/ejercicio1.js
@@ -0,0 +1,11 @@
+console.info('MENSAJE DE INFO');
+
+console.warn('MENSAJE DE WARNING');
+
+console.error('MENSAJE DE ERROR');
+
+console.log('MENSAJE DE LOG');
+
+console.time('Tiempo de ejecución');
+
+console.timeEnd('Tiempo de ejecución');
