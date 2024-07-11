a961183eeed1a1c81b363f860d68f677de6452bd
Daniel de Andrés <deandreslopezdaniel@gmail.com>
2024-07-11 17:20:25 +0200

    ejercicio 3

---
+++ JavaScript/ejercicio3.js
@@ -0,0 +1,19 @@
+
+//A
+var cadena1 = "CADENA1";
+
+//B
+console.log(cadena1.indexOf("ENA"));
+
+//C
+console.log(cadena1.charAt(cadena1.length-1));
+
+//D
+console.log(cadena1.toLowerCase());
+
+//E
+console.log(cadena1.replace("DE", "ME"));
+
+//F
+var cadena2 = "CADENA2";
+console.log(cadena1+cadena2);
