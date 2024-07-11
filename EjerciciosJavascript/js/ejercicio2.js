f8671c2d349e9aac43bdc64dc954d484217f8715
Alejandro <soutofernandezalejandro@gmail.com>
2024-07-11 16:44:48 +0200

    2

---
+++ EjerciciosJavascript/js/ejercicio2.js
@@ -0,0 +1,12 @@
+// a)
+let alto = 12;
+let ancho = 12.203004;
+
+// b)
+let altoPorAncho = alto * ancho;
+
+// c)
+console.log('Valor de altoPorAncho:', altoPorAncho);
+
+// d)
+console.log('Valor de altoPorAncho redondeado a 2 decimales:', altoPorAncho.toFixed(2));
