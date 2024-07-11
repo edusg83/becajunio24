1258274e94457c5b4afa4b22523bfa3c8f7b291d
Daniel de Andrés <deandreslopezdaniel@gmail.com>
2024-07-11 16:47:46 +0200

    JavaScript

---
+++ JavaScript/ejercicio1.js
@@ -0,0 +1,9 @@
+console.info("Mensaje de info");
+
+console.warn("Mensaje de warning");
+
+console.error("Mensaje de error");
+
+console.log("Mensaje de info");
+
+console.log(new Date().toLocaleTimeString());
---
+++ JavaScript/ejercicio2.js
@@ -0,0 +1,8 @@
+
+var alto = 12;
+var ancho = 12.203004;
+var altoPorAncho = alto*ancho;
+
+console.info(altoPorAncho);
+console.info(altoPorAncho.toFixed(2));
+
