d1579f319e91ff6d0814f19ddeb3c61d719e1989
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2024-07-11 18:46:05 +0200

    ejercicio4

---
+++ jsTest/ejercicio4.js
@@ -0,0 +1,5 @@
+var cadena1 = "CADENA1";
+var variableU;
+typeof variableU;
+
+typeof(variableU);
---
+++ jsTest/ejercicio5.js
@@ -0,0 +1,77 @@
+var array = new Array();
+array = [1,2,3,4];
+
+var length = array.length;
+for(var i = 0; i < length; i++)
+{
+    switch(i)
+    {
+        case 0:
+            console.log("UNO")
+            break;
+
+        case 1:
+             console.log("DOS")
+            break;
+
+        case 2:
+            console.log("TRES")
+            break;
+
+        case 3:
+            console.log("CUATRO")
+            break;     
+    }
+}
+
+var array2 = new Array();
+array2 = [1,2,3,4,3];
+
+var length2 = array2.length;
+for(var i = 0; i < length2; i++)
+{
+    switch(i)
+    {
+        case 0:
+            console.log("UNO")
+            break;
+
+        case 1:
+             console.log("DOS")
+            break;
+
+        case 2:
+            console.log("TRES")
+            break;
+
+        case 3:
+            console.log("CUATRO")
+            break;     
+    }
+}
+
+var array3 = new Array();
+array3 = [1,2];
+
+var length3 = array3.length;
+for(var i = 0; i < length3; i++)
+{
+    switch(i)
+    {
+        case 0:
+            console.log("UNO")
+            break;
+
+        case 1:
+             console.log("DOS")
+            break;
+
+        case 2:
+            console.log("TRES")
+            break;
+
+        case 3:
+            console.log("CUATRO")
+            break;     
+    }
+}
