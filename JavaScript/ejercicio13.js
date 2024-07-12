867d8679c9c725b1ec11849cb1f2a2662e728d48
Daniel de Andrés <deandreslopezdaniel@gmail.com>
2024-07-12 17:27:09 +0200

    13 arreglado

--- JavaScript/ejercicio13.js
+++ JavaScript/ejercicio13.js
@@ -2,9 +2,5 @@
 try {
     throw new EvalError('Se ha producido un error', 'ejercicio10.js', 100);
   } catch (e) {
-    if (e instanceof EvalError) {
-      console.error(`EvalError: ${e.message} en ${e.fileName}, línea ${e.lineNumber}`);
-    } else {
-      console.error(`Error: ${e.message}`);
-    }
+      console.error(`EvalError: ${e.message} en ${e.fileName}, línea ${e.lineNumber}`); 
   }
