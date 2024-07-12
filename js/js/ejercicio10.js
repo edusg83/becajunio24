d933ebf0054edfcdf2b62ec449fed22c28358f02
AlvaroBuenoSosa <130675481+AlvaroBuenoSosa@users.noreply.github.com>
2024-07-12 16:20:06 +0200

    Add files via upload

---
+++ js/js/ejercicio10.js
@@ -0,0 +1,12 @@
+let funciones = {
+    colorVerde: function() {
+      console.log("VERDE");
+    },
+
+    colorRojo: function(){
+        console.log("ROJO");
+    }   
+};
+  
+funciones.colorVerde();
+funciones.colorRojo();
