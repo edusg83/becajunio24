821108e01359352232c46bb04bed16aba030f146
Alejandro <soutofernandezalejandro@gmail.com>
2024-07-12 17:20:19 +0200

    1

---
+++ EjerciciosJavascript/js/ejercicio13.js
@@ -0,0 +1,9 @@
+// a)
+try {
+    // b)
+    throw new EvalError('Se ha producido un error', 'ejercicio13.js', 100);
+} catch (error) {
+    console.error(error.name + ': ' + error.message);
+    console.error('Fichero: ' + error.fileName);
+    console.error('Línea: ' + error.lineNumber);
+}
