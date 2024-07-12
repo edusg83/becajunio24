6a6b3de47148488b4e608a98de499e900b43a4ae
Alejandro <soutofernandezalejandro@gmail.com>
2024-07-12 19:05:26 +0200

    1

---
+++ EjerciciosJavascript/js/ejercicio20.js
@@ -0,0 +1,5 @@
+let nuevoParrafo = document.createElement('p');
+nuevoParrafo.textContent = "Este texto está añadido dinámicamente";
+document.body.appendChild(nuevoParrafo);
+
+console.log('Elemento <p> añadido:', nuevoParrafo);
