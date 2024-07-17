ab4d0aade69db015987d4d4fb7a4aa205e26068c
aann_2 <ana.aguilera.lopez02@gmail.com>
2024-07-17 19:22:06 +0200

    ejercicio 4

---
+++ API/js/ejercicio3.js
@@ -0,0 +1,28 @@
+axios.get('https://eoi.wiremockapi.cloud/users')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        displayFirstUser(data.arrayUsuarios[0]);
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
+
+function displayFirstUser(user) {
+    document.getElementById('user-name').textContent = `${user.nombre} ${user.apellidos}`;
+
+    const tableBody = document.querySelector("#address-table tbody");
+    user.direcciones.forEach(direccion => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+            <td>${direccion.direccion}</td>
+            <td>${direccion.cpostal}</td>
+        `;
+        tableBody.appendChild(row);
+    });
+}
